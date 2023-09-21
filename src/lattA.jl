2201b04336bcacb9330dd909ad94762f07e2e14c
ale <asaez_@hotmail.com>
2023-09-21 23:13:02 +0200

    add export get_m

--- src/lattA.jl
+++ src/lattA.jl
@@ -8,4 +8,10 @@ export EnsInfo
 include("reader.jl")
 export get_corr_wil
 
+include("obs.jl")
+export get_m
+
+include("tools.jl")
+export model_av
+
 end
--- test/test_wilson.jl
+++ test/test_wilson.jl
@@ -10,6 +10,6 @@ ens = EnsInfo(id, ens_db[id])
 path = "/home/asaez/cls_ens/data"
 
 pp, ppw, w = get_corr_wil(path, ens, "G5", "G5", rw=true, info=true, legacy=true)
-pp_sym = [corr_sym(pp[i],pp[i+1],+1) for i in 1:2:length(pp)-1]
+pp_sym = [corr_sym(pp[i],pp[i+1],+1) for i in 1:2:length(pp)-1];
 
 mpi = get_m(pp_sym[1], id)
