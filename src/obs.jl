953d3928ac68d71d6f1a296ac9b7558234e28bd4
Alejandro Saez <asaez@ciclope2.local>
2023-09-22 11:19:10 +0200

    start mpcac

--- src/obs.jl
+++ src/obs.jl
@@ -1,18 +1,20 @@
 using juobs, LaTeXStrings, PyPlot
 include("const.jl");
 
-function get_m(corr::juobs.Corr, id::String; tm::Union{Vector{Int64}, Nothing}=nothing, tM::Union{Vector{Int64}, Nothing}=nothing, pl::Bool=false, wpm::Union{Dict{Int64,Vector{Float64}},Dict{String,Vector{Float64}}, Nothing}=nothing)
+function get_m(corr::juobs.Corr, ens::EnsInfo; tm::Union{Vector{Int64}, Nothing}=nothing, tM::Union{Vector{Int64}, Nothing}=nothing, pl::Bool=false, wpm::Union{Dict{Int64,Vector{Float64}},Dict{String,Vector{Float64}}, Nothing}=nothing)
     corr_d = corr.obs
     m_dat = 0.5 .* log.((corr_d[2:end-2] ./ corr_d[3:end-1]) .^ 2)
     y0 = corr.y0
-    T = length(corr_d) - y0
+    T = length(corr_d) - 1 - y0
     
     isnothing(tm) ? tm = [y0+10,y0+12,y0+14] : tm=tm
     isnothing(tM) ? tM = [T-10,T-12,T-14] : tM=tM
-    @.fit_fun(x,p) = p[1] + p[2] * exp(-p[3] * (x-y0)) + p[4] * exp(-p[5] * (T-x))
-    k = 5
+    @.fit_exp(x,p) = p[1] + p[2] * exp(-p[3] * (x-y0)) + p[4] * exp(-p[5] * (T-x))
+    @.fit_const(x,p) = p[1] * x ^ 0
+    k1 = 1
+    k2 = 5
     
-    m, syst, m_i, weight, pval = model_av(fit_fun, m_dat, ens_obs[id][2], tm=tm, tM=tM, k=k, wpm=wpm)
+    m, syst, m_i, weight, pval = model_av([fit_const, fit_exp], m_dat, ens_db[ens.id][2], tm=tm, tM=tM, k=[k1,k2], wpm=wpm)
     if pl == true
         isnothing(wpm) ? uwerr(m) : uwerr(m, wpm)                       
 	isnothing(wpm) ? uwerr.(m_dat) : [uwerr(m_dat[i], wpm) for i in 1:length(m_dat)]
@@ -26,7 +28,7 @@ function get_m(corr::juobs.Corr, id::String; tm::Union{Vector{Int64}, Nothing}=n
         errorbar(1:length(m_dat), value.(m_dat), err.(m_dat), fmt="x", color="black")
         ylabel(L"$m_\mathrm{eff}$")
         xlabel(L"$x_0$")
-        ylim(v-50*e, v+100*e)
+        ylim(v-20*e, v+50*e)
 
 	subplot(412)
 	ylabel(L"$m_i$")
@@ -46,4 +48,21 @@ function get_m(corr::juobs.Corr, id::String; tm::Union{Vector{Int64}, Nothing}=n
     end
 
     return m, syst, m_i, weight, pval
+end
+
+function get_mpcac(corr_pp::juobs.Corr, corr_ap::juobs.Corr, ens::EnsInfo; tm::Union{Vector{Int64}, Nothing}=nothing, tM::Union{Vector{Int64}, Nothing}=nothing, impr::Bool=true, pl::Bool=false, wpm::Union{Dict{Int64,Vector{Float64}},Dict{String,Vector{Float64}}, Nothing}=nothing)
+
+    ap_dat = -corr_ap.obs
+    pp_dat = corr_pp.obs
+    der_ap = (ap_dat[3:end] .- ap_dat[1:end-2]) / 2
+    if impr == true
+        ca = ens.ca
+        der2_pp = (pp_dat[1:end-4] - 2*pp_dat[3:end-2] + pp_dat[5:end]) / 4
+        der_ap = der_ap[2:end-1] + ca * der2_pp
+    end
+    mpcac_dat = der_ap ./ pp_dat[3:end-2]
+    
+    y0 = corr.y0
+    T = length(corr_d) - y0
+        
 end
--- src/tools.jl
+++ src/tools.jl
@@ -5,19 +5,21 @@ function fit_defs(f::Function,x,W) ## uncorrelated fit
 	return chisq
 end
 
-function model_av(f::Function, y::Vector{uwreal}, guess::Float64; tm::Vector{Int64}, tM::Vector{Int64}, k::Int64, wpm::Union{Dict{Int64,Vector{Float64}},Dict{String,Vector{Float64}}, Nothing}=nothing) 
+function model_av(fun::Vector{Function}, y::Vector{uwreal}, guess::Float64; tm::Vector{Int64}, tM::Vector{Int64}, k::Vector{Int64}, wpm::Union{Dict{Int64,Vector{Float64}},Dict{String,Vector{Float64}}, Nothing}=nothing) 
     pval = Array{Float64,1}()
     p_1 = Array{uwreal,1}()
     TIC = Array{Float64,1}()
 
     isnothing(wpm) ? uwerr.(y) : [uwerr(y[i], wpm) for i in 1:length(y)]
+    for ind in 1:length(fun)
+    f = fun[ind]
     for i in tm
         for j in tM
             x = collect(i:j)
             y_aux = y[i:j]
             dy = err.(y_aux)
             W = 1 ./ dy .^ 2
-            p0 = [guess; [0.5 for i in 1:k-1]]
+            p0 = [guess; [0.5 for i in 1:k[ind]-1]]
             chisq = fit_defs(f,x,W)
             fit = curve_fit(f,x,value.(y_aux),W,p0)
             chi2 = sum(fit.resid .^ 2)
@@ -26,6 +28,7 @@ function model_av(f::Function, y::Vector{uwreal}, guess::Float64; tm::Vector{Int
             push!(TIC, chi2 - 2*chi_exp)
             push!(p_1, up[1])
         end
+    end
     end
 
     TIC = TIC .- minimum(TIC)
