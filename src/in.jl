979a4f9ec6e78a40c41198733a9365ec726d50e8
Alejandro Saez <asaez@ciclope2.local>
2024-03-06 12:11:23 +0100

    change in.jl to t0 iter again

--- src/in.jl
+++ src/in.jl
@@ -2,16 +2,16 @@ using juobs, BDIO, ADerrors
 
 include("/home/asaez/cls_ens/codes/lattA.jl/src/const.jl");
 
-phi4_ph = uwreal([1.11,0.0],"phi4_ph")
-sqrt_t0_ph = sqrt(phi4_ph / 8 / (MK ^ 2 + 0.5 * Mpi ^ 2) * hc ^ 2)
-t0_ph = sqrt_t0_ph ^ 2
+#phi4_ph = uwreal([1.11,0.0],"phi4_ph")
+#sqrt_t0_ph = sqrt(phi4_ph / 8 / (MK ^ 2 + 0.5 * Mpi ^ 2) * hc ^ 2)
+#t0_ph = sqrt_t0_ph ^ 2
 
-#fb = BDIO_open("/home/asaez/cls_ens/results/t0.bdio", "r") 
-#BDIO_seek!(fb) 
-#BDIO_seek!(fb, 2)
-#BDIO_seek!(fb, 2)  
-#t0_ph = read_uwreal(fb)
-#sqrt_t0_ph = sqrt(t0_ph)
+fb = BDIO_open("/home/asaez/cls_ens/results/t0.bdio", "r") 
+BDIO_seek!(fb) 
+BDIO_seek!(fb, 2)
+BDIO_seek!(fb, 2)  
+t0_ph = read_uwreal(fb)
+sqrt_t0_ph = sqrt(t0_ph)
 
 #sqrt_t0_ph = uwreal([0.1445,0.0],"t0_ph")
 
