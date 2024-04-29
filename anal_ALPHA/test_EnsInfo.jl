339561ca9ee8ce3d5a8d20394a2811be85ecc53e
ale <asaez_@hotmail.com>
2024-04-29 10:41:01 +0200

    remove ALPHA anal

--- anal_ALPHA/test_EnsInfo.jl
+++
@@ -1,9 +0,0 @@
-import Pkg
-Pkg.activate("/home/alejandrosaez/University/PhD/codes/lattA.jl")
-
-using lattA
-
-include("/home/alejandrosaez/University/PhD/codes/lattA.jl/src/const.jl")
-
-id = "H101"
-ens = EnsInfo(id, ens_db[id])
