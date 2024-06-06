fbd83b79e394cd4144c1552e977ec2a960aba984
ale <asaez_@hotmail.com>
2024-06-06 11:47:44 +0200

    bug

--- src/reader.jl
+++ src/reader.jl
@@ -465,7 +465,7 @@ function get_YM(path::String, ens::EnsInfo; rw=false, ws::ADerrors.wspace=ADerro
             tdt2YM[i,1] = tdt2YM[i,end] = t2YM[i,1]
         else
             ix = findmin(abs.(t .- w0))[2]
-            tdt2YM[i,1:end] = t2YM[i,1]
+            tdt2YM[i,1:end] .= t2YM[i,1]
             tdt2YM[i,ix-3:ix+3] = [(t2YM[i,j+1] - t2YM[i,j-1]) / (t[j+1] - t[j-1]) * t[j] for j in ix-3:ix+3]
         end
     end
