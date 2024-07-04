a3489f0735caf5d093eecb256ed2abeaedcc34f7
Alejandro Saez <asaez@ciclope2.local>
2024-07-04 23:10:08 +0200

    bla

--- src/in.jl
+++ src/in.jl
@@ -6,15 +6,15 @@ include("/home/asaez/cls_ens/codes/lattA.jl/src/const.jl");
 #sqrt_t0_ph = sqrt(phi4_ph / 8 / (MK ^ 2 + 0.5 * Mpi ^ 2) * hc ^ 2)
 #t0_ph = sqrt_t0_ph ^ 2
 
-#fb = BDIO_open("/home/asaez/cls_ens/results/t0_2nd.bdio", "r") 
-#BDIO_seek!(fb) 
-#BDIO_seek!(fb, 2)
-#BDIO_seek!(fb, 2)  
-#t0_ph = read_uwreal(fb)
-#sqrt_t0_ph = sqrt(t0_ph)
+fb = BDIO_open("/home/asaez/cls_ens/results/t0_3.bdio", "r") 
+BDIO_seek!(fb) 
+BDIO_seek!(fb, 2)
+BDIO_seek!(fb, 2)  
+t0_ph = read_uwreal(fb)
+sqrt_t0_ph = sqrt(t0_ph)
 
-sqrt_t0_ph = uwreal([0.1438,0.0],"t0_ph")
-t0_ph = sqrt_t0_ph ^ 2
+#sqrt_t0_ph = uwreal([0.1438,0.0],"t0_ph")
+#t0_ph = sqrt_t0_ph ^ 2
 
 phi4_ph = 8 * sqrt_t0_ph ^ 2 * (MK ^ 2 + 0.5 * Mpi ^ 2) / hc ^ 2
 phi2_ph = 8 * sqrt_t0_ph ^ 2 * Mpi ^ 2 / hc ^ 2
