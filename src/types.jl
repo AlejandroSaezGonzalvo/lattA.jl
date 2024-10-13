c3c0e2efdf1ff2be116fe83a689b30c9da1ec1d8
ale <asaez_@hotmail.com>
2024-10-13 20:52:55 +0200

    add ca uwreal, but commented out

--- src/types.jl
+++ src/types.jl
@@ -1,3 +1,37 @@
+#=
+
+using ADerrors
+
+mutable struct EnsInfo
+    id::String
+    L::Int64
+    T::Int64
+    beta::Float64
+    ca::uwreal
+    dtr::Int64
+    vrw::Union{String, Vector{String}}
+    cnfg::Vector{Int64}
+    function EnsInfo(ens_id::String, info::Vector{Any})
+        id = ens_id
+        L = info[1]
+        T = info[2]
+        beta = info[3]
+        dtr = info[4]
+        vrw = info[5]
+	    cnfg = info[6]
+
+        p0 = 9.2056
+        p1 = -13.9847
+        g2 = 6 ./ beta 
+        ca = - 0.006033 .* g2 .*( 1 .+exp.(p0 .+ p1./g2)) 
+        ca_err = 0.08 * ca
+        ca_uw = uwreal([ca, ca_err], string("ca-", ens_id))
+        return new(id, L, T, beta, ca_uw, dtr, vrw, cnfg)
+    end
+end
+
+=#
+
 mutable struct EnsInfo
     id::String
     L::Int64
